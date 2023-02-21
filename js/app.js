9851939dd82932fc58c222be872587e285aaf99f
Jennifer Li Dotson <jenniferlidotson@gmail.com>
2023-02-21 15:25:58 -0800

    added bar chart

--- js/app.js
+++ js/app.js
@@ -163,43 +163,33 @@ function viewChart(){
         label: 'Number of Votes',
         data: itemVotes,
         backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
+          'pink',
+          'aqua',
           'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'lime',
+          'lavender',
+          'salmon'
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'black'
         ],
-        borderWidth: 1
+        borderWidth: 3
       },
       {
         label: 'Number of Views',
         data: itemViews,
         backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'red',
+          'blue',
+          'yellow',
+          'green',
+          'purple',
+          'orange'
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'white'
         ],
-        borderWidth: 1
+        borderWidth: 3
       }]
     },
     options: {
